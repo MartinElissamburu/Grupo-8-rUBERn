8aac3d3464fcadd37ee4daee5062778778f4d14e
Martin Elissamburu <martin.elissamburu@ing.austral.edu.ar>
2016-10-26 19:01:57 -0300

    Saque todo lo que habia demas en el main

--- src/rUBERn.java
+++ src/rUBERn.java
@@ -23,65 +23,6 @@ public class rUBERn {
         }
 
 
-        while (true) {
-
-            System.out.println("------------\n" +
-                    "Que operacion desea realizar:\n" +
-                    "1. Realizar un viaje\n" +
-                    "2. Enunciar choferes\n" +
-                    "3. Enunciar usuarios\n" +
-                    "4. Salir\n");
-            String impresion = null;
-            int impresion2 = 888888;
-            int j = Scanner.getInt("Que operación desea realizar: ");
-            switch (j) {
-                case 1:
-
-                    break;
-                case 2:
-
-                    break;
-                case 3:
-
-                    break;
-                case 4:
-                    impresion = "Gracias por utilizar este programa";
-                    impresion2 = 9999999;
-                    break;
-                default:
-                    impresion = "Esta opcion no es valida";
-                    impresion2 = 0;
-                    break;
-            }
-            if (!impresion.equals("6")) {
-                if (impresion2 != 9999999) {
-                    System.out.println("");
-                    System.out.println(impresion);
-                    if (impresion2 != 888888) {
-                        System.out.println(impresion2);
-                        System.out.println("");
-                    }
-                } else {
-                    System.out.println("");
-                    System.out.println(impresion);
-                    System.out.println("");
-                    System.exit(0);
-                }
-            } else {
-                System.out.println("No lo hice ese, era opcional");
-            }
-            int limpiar = Scanner.getInt("Si desea limpiar la consola ingrese 1, si desea que la consola quede " +
-                    "sin limpiar ingrese 2 y si desea salir ingrese 3");
-            if (limpiar == 1) {
-                System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" +
-                        "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-            } else if (limpiar == 3) {
-                System.out.println("Gracias por usar este programa");
-                System.exit(0);
-            } else {
-                System.out.println("");
-            }
-        }
     }
 }
 
